89730b1c5df5a55e91ce46b881fa27d576d444b0
dinhtuannam <namdinhtuan5@gmail.com>
2026-05-12 18:47:10 +0700

    u

---
+++ MY_EC51/MY_EC51App.swift
@@ -0,0 +1,17 @@
+//
+//  MY_EC51App.swift
+//  MY_EC51
+//
+//  Created by MacOS on 12/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct MY_EC51App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
